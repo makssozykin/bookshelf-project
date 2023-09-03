fc0632ef91467fb147333cb93aab77d95baa14d6
makssozykin <mvsozykin@gmail.com>
2023-09-03 17:16:22 +0300

    books add to localstoradge

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -18,22 +18,15 @@ function booksData({ data }) {
     try { 
         data.forEach(book_cat => {
             const { list_name, books } = book_cat;
-            // console.log('book_cat:', books);
-            if (!localStorage.getItem(LOCALSTORADGE_KEY)) {
-                empty.style.display = 'flex';
-                bookItem.style.display = 'none';
-            }
-            // else {
-            //     books.forEach(book => {
-            //         // let bookInfo = { _id, list_name, book_image, title, description, author, amazon_product_url };
-            //         // book = bookInfo;
-            //         console.log('bookID', book);
-                
-                
-                    
-            //         localStorage.setItem(LOCALSTORADGE_KEY, JSON.stringify(booksArr));
-            //     });
-            // }
+            console.log('book_cat:', books);
+                books.forEach(book => {
+                    // let bookInfo = { _id, list_name, book_image, title, description, author, amazon_product_url };
+                    // book = bookInfo;
+                    console.log('bookID', book);
+                    booksArr.push(book);
+                    localStorage.setItem(LOCALSTORADGE_KEY, JSON.stringify(booksArr));
+                });
+
             });
         }
     catch (error) {
